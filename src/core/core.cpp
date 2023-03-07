a0588bf9c318642305481d103d1c8f58a363f712
wxz <2565439576@qq.com>
2023-03-07 22:25:32 +0800

    编写控制模块

---
+++ src/core/core.cpp
@@ -0,0 +1,19 @@
+#include "core.h"
+
+int countChains(char ** words, int length, char * result[]) {
+
+}
+
+int getLongestWordChain(char ** words, int length, char * result[], char head, 
+                        char tail, char ban, bool allow_circle) {
+    
+}
+
+int getLongestCharChain(char ** words, int length, char * result[], char head, 
+                        char tail, char ban, bool allow_circle) {
+    
+}
+
+int main() {
+    
+}
